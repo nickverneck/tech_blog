edfdbc37639b9b2cab506f348ada80f45999411d
Nick Verneck <nicollas@gmail.com>
2021-05-14 00:23:56 -0400

    added models and relations

---
+++ models/index.js
@@ -0,0 +1,23 @@
+const Post = require('./post');
+const Comment = require('./comment');
+const User = require('./user');
+
+
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: "CASCADE"
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+});
+
+
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id',
+    onDelete: "CASCADE"
+})
+
+module.exports = { Post, Comment, User };
